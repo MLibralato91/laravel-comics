c69b0f366364398e400efdb71b679e80f5c443e0
MLibralato91 <michelelibralato911@gmail.com>
2023-05-22 17:57:14 +0200

    update styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,47 @@
     <main>
         <div class="container">
             <div class="row">
-                @foreach ($products as $product)
-                    <div class="col-3">
-                        <div class="card">
-                            <img src="{{ $product['thumb'] }}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    <li>{{ $product['title'] }}</a>
-                                </h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                    of the card's
-                                    content.</p>
-                                <a href="{{ route('products.show', ['id' => $loop->index]) }}" class="btn btn-primary">Go
-                                    somewhere</a>
-                            </div>
-                        </div>
+
+                <main>
+                    <div class="jumbotron">
+                        <img class="" src="../../public/images/dc-logo.png" alt="">
+
                     </div>
-                @endforeach
+                    <div class="container position-relative ">
+                        <div class="current text-uppercase fw-bolder bg-primary position-absolute ">
+                            Current Series
+                        </div>
+                        <div class="row pt-4">
+                            @foreach ($products as $product)
+                                <div class="col-3">
+                                    <div class="card">
+                                        <img src="{{ $product['thumb'] }}" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">
+                                                <li>{{ $product['title'] }}</a>
+                                            </h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make
+                                                up the bulk
+                                                of the card's
+                                                content.</p>
+                                            <a href="{{ route('products.show', ['id' => $loop->index]) }}"
+                                                class="btn btn-primary">Go
+                                                somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+
+                    </div>
+                    <div class="text-center p-3">
+                        <button type="button" class="bg-primary text-white small px-5 py-1">
+                            LOAD MORE
+                        </button>
+
+                    </div>
+                </main>
+
             </div>
         </div>
     </main>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $product['title'] }}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 text-center m-auto">
+                <img src="{{ $product['thumb'] }}" alt="">
+                <h3>{{ $product['title'] }}</h3>
+                <p>{{ $product['description'] }}</p>
+            </div>
+        </div>
+    </div>
 @endsection
