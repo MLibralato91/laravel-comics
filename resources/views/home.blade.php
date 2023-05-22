dc6a3d15148572ec03171f2d7ada1582ef7c11f2
MLibralato91 <michelelibralato911@gmail.com>
2023-05-22 16:51:50 +0200

    fix foreach and add image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
     <main>
-        @foreach ($products as $product)
-            {{-- <li><a href="{{ route('products.show', ['id' => $loop->index]) }}">{{ $product['title'] }}</a></li> --}}
-        @endforeach
+        <div class="container">
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-3">
+                        <div class="card">
+                            <img src="{{ $product['thumb'] }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <li>{{ $product['title'] }}</a>
+                                </h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's
+                                    content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 @endsection
